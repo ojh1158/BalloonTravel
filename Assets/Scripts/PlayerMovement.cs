a31d76a58f657c1a960bc5ae1dbb486154472415
tlsehddbs <sdy4277@naver.com>
2022-06-27 17:49:53 +0900

    Character Movement, Following Camera Setting
    add two island, character, character Movement, character Animations(running), Following camera
    etc.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    Vector3 horizontalMovement, verticalMovement;
+
+    void Start()
+    {
+        horizontalMovement = Camera.main.transform.forward;
+        horizontalMovement.y = 0;
+        horizontalMovement = Vector3.Normalize(horizontalMovement);
+        verticalMovement = Quaternion.Euler(new Vector3(0, 90, 0)) * horizontalMovement;
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.anyKey)
+            Move();
+    }
+
+    void Move()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 rightMovement = verticalMovement * speed * Time.deltaTime * Input.GetAxis("Horizontal");
+        Vector3 upMovement = horizontalMovement * speed * Time.deltaTime * Input.GetAxis("Vertical");
+
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+
+        transform.forward = heading;
+        transform.position += rightMovement;
+        transform.position += upMovement;
+    }
+
+}

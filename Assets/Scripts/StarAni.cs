1bce27ec7a9ec7156f6ae2fd6e0b986e6a19a58b
ojh1158 <108050056+ojh1158@users.noreply.github.com>
2022-07-18 07:44:29 +0900

    12312321
    12411412911ㄹ아ㅡㅏ느ㅏㄹ

--- Assets/Scripts/StarAni.cs
+++ Assets/Scripts/StarAni.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StarAni : MonoBehaviour
 {
    
     public GameObject star_ain;
     public GameObject back;
+    public Image fade;
     public GameObject[] star_get;
 
     public int Dialog_Content; // 내부 대화 내용
@@ -16,9 +18,18 @@ public class StarAni : MonoBehaviour
     public GameObject textmanager;
     TextManager gamemanager;
 
+    bool isTlaking;
+
+    float fadeCount = 0f;
     int star_get_int = 0;
 
-
+    void Update()
+    {
+        if (isTlaking)
+        {
+            GameManager.isTalking = true;
+        }
+    }
 
     public void Star_Get()
     {
@@ -27,14 +38,17 @@ public class StarAni : MonoBehaviour
 
     IEnumerator WaitTime()
     {
+        isTlaking = true;
+        yield return new WaitForSeconds(0.5f);
         back.SetActive(true);
         star_ain.SetActive(true);
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(2.2f);
         back.SetActive(false);
+        StartCoroutine(FadeCoroutine());
         star_ain.SetActive(false);
         Debug.Log(star_get_int);
         star_get[star_get_int].SetActive(true);
-        yield return new WaitForSeconds(0.7f);
+        yield return new WaitForSeconds(1.5f);
         StartCoroutine(Start_Talk());
         star_get_int++;
     }
@@ -42,8 +56,26 @@ public class StarAni : MonoBehaviour
     IEnumerator Start_Talk()
     {
         yield return null;
+        isTlaking = false;
+        GameManager.isTalking = true;
         gamemanager = textmanager.GetComponent<TextManager>();                                      //참조를 위한 재선헌
         StartCoroutine(gamemanager.Dialogue(Dialog_Name, Dialog_Content, Dialog_FinerContent));     //코루틴 시작 함수
+        
     }
-    
+
+    IEnumerator FadeCoroutine()
+    {
+        fadeCount = 1f;
+        while (true)
+        {
+            fadeCount -= 0.01f;
+            yield return new WaitForSeconds(0.01f);     //0.01초마다 실행
+
+            fade.color = new Color(255, 255, 255, fadeCount);
+            if (fadeCount < 0.0f)
+            {
+                yield break;
+            }
+        }
+    }
 }
--- Assets/Scripts/Text/Text_Button.cs
+++ Assets/Scripts/Text/Text_Button.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class Text_Button : MonoBehaviour
 {
-    public GameObject textmanager; // 텍스트 매니저 
-    public GameObject player; // 플레이어 옮기는 용도
+    public GameObject textmanager;  // 텍스트 매니저 
+    public GameObject player;       // 플레이어 옮기는 용도
 
-    public int Dialog_Content; // 내부 대화 내용
-    public int Dialog_Name; // 내부 인물 이름
+    public int Dialog_Content;      // 내부 대화 내용
+    public int Dialog_Name;         // 내부 인물 이름
     public int Dialog_FinerContent; // 마지막 페이지
 
-    public bool Button_Actvie;
-    public bool O; //정답 버튼
-    public bool button_O = true;
+    [HideInInspector] public bool Button_Actvie;
+    public bool O;                  //정답 버튼
     
 
     
@@ -25,10 +24,6 @@ public class Text_Button : MonoBehaviour
         {
             StartCoroutine(buttenManager());
             GameManager.isTalking = true;
-            if (!button_O)
-            {
-                GameManager.isTalking = false;
-            }
         }
     }
     IEnumerator buttenManager()
@@ -67,17 +62,18 @@ public class Text_Button : MonoBehaviour
         }
         if (O)
         {
-            while (button_O)
+            while (true)
             {
+                GameManager.isTalking = true;
                 yield return null;
                 transform.position += down;
                 temp += i;
 
                 if (temp > 0.2f)
                 {
+                    GameManager.isTalking = true;
                     //gamemanager = textmanager.GetComponent<TextManager>();                                      //참조를 위한 재선헌
                     //StartCoroutine(gamemanager.Dialogue(Dialog_Name, Dialog_Content, Dialog_FinerContent));     //코루틴 시작 함수
-                    button_O = false;
                     Button_Actvie = false;
                     yield break;
                 }
